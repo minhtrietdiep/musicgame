ceaab75cc903cc8775e10faa75aedd30f282fc2b
Thomas Schraven <t.schraven@student.fontys.nl>
2015-01-08 12:54:13 +0100

    added ButtonDebouncer to main .ino

--- MusicGame.ino
+++ MusicGame.ino
@@ -1,13 +1,21 @@
-const long connectionSpeed = 115200;
+#include "ButtonDebouncer.h"
+
+static const int BUTTON_AMOUNT = 3;
+static const long connectionSpeed = 115200;
+
 const int buzzerPin = 3;
 const int slider1 = A2;
 const int slider2 = A1;
 const int slider3 = A0;
-const int button1 = 10;
-const int button2 = 11;
-const int button3 = 12;
 const int led5 = 5;
 const int led6 = 6;
+
+ButtonDebouncer buttons[BUTTON_AMOUNT] =
+{
+	ButtonDebouncer(10, INPUT_PULLUP),
+	ButtonDebouncer(11, INPUT_PULLUP),
+	ButtonDebouncer(12, INPUT_PULLUP)
+};
 
 int difference;
 int currentMusicTone;
@@ -20,9 +28,6 @@ void setup()
 	Serial.begin(connectionSpeed);
 	pinMode(led5, OUTPUT);
 	pinMode(led6, OUTPUT);
-	pinMode(button1, INPUT_PULLUP);
-	pinMode(button2, INPUT_PULLUP);
-	pinMode(button3, INPUT_PULLUP);
 }
 
 void loop()
