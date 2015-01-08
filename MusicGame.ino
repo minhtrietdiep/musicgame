029b6aea92d144ebbfbbfb8930fb71676504a9a1
Thomas Schraven <t.schraven@student.fontys.nl>
2015-01-08 17:19:55 +0100

    added MusicListBuilder, made const variables static

--- MusicGame.ino
+++ MusicGame.ino
@@ -1,14 +1,19 @@
 #include "ButtonDebouncer.h"
+#include "MusicFile.h"
+#include "MusicListBuilder.h"
+#include "Pitches.h"
 
 static const int BUTTON_AMOUNT = 3;
-static const long connectionSpeed = 115200;
+static const int MAX_MUSIC = 20;
 
-const int buzzerPin = 3;
-const int slider1 = A2;
-const int slider2 = A1;
-const int slider3 = A0;
-const int led5 = 5;
-const int led6 = 6;
+static const int connectionSpeed = 115200;
+
+static const int buzzerPin = 3;
+static const int slider1 = A2;
+static const int slider2 = A1;
+static const int slider3 = A0;
+static const int led5 = 5;
+static const int led6 = 6;
 
 ButtonDebouncer buttons[BUTTON_AMOUNT] =
 {
@@ -17,14 +22,19 @@ ButtonDebouncer buttons[BUTTON_AMOUNT] =
 	ButtonDebouncer(12, INPUT_PULLUP)
 };
 
+MusicFile MusicFiles[MAX_MUSIC];
+MusicListBuilder Builder;
+
 int difference;
 int currentMusicTone;
 int currentUserTone;
-int songArray[20];
 String s;
 
 void setup()
 {
+	Builder = MusicListBuilder(MAX_MUSIC, MAX_NOTES, buzzerPin);
+	Builder.Build(MusicFiles);
+
 	Serial.begin(connectionSpeed);
 	pinMode(led5, OUTPUT);
 	pinMode(led6, OUTPUT);
@@ -38,7 +48,7 @@ void loop()
 	// IsButtonOnePressed = button[0].IsPressed;
 
 	// simply builds the string
-	if (Serial.available() > 0) 
+	if (Serial.available() > 0)
 	{
 		s += (char)Serial.read();
 	}
