c23b46a7e52eb7eb8303f48380b28426f6eb95e3
MT <minhtrietdiep@gmail.com>
2015-01-09 12:14:35 +0100

    C# Things

--- MusicGameServer/MusicGameForm.Designer.cs
+++ MusicGameServer/MusicGameForm.Designer.cs
@@ -28,13 +28,40 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
+            this.readMessageTimer = new System.Windows.Forms.Timer(this.components);
+            this.musicSelectionBox = new System.Windows.Forms.ComboBox();
+            this.setMusicButton = new System.Windows.Forms.Button();
             this.SuspendLayout();
+            // 
+            // readMessageTimer
+            // 
+            this.readMessageTimer.Tick += new System.EventHandler(this.readMessageTimer_Tick);
+            // 
+            // musicSelectionBox
+            // 
+            this.musicSelectionBox.FormattingEnabled = true;
+            this.musicSelectionBox.Location = new System.Drawing.Point(12, 35);
+            this.musicSelectionBox.Name = "musicSelectionBox";
+            this.musicSelectionBox.Size = new System.Drawing.Size(121, 21);
+            this.musicSelectionBox.TabIndex = 0;
+            // 
+            // setMusicButton
+            // 
+            this.setMusicButton.Location = new System.Drawing.Point(13, 63);
+            this.setMusicButton.Name = "setMusicButton";
+            this.setMusicButton.Size = new System.Drawing.Size(75, 23);
+            this.setMusicButton.TabIndex = 1;
+            this.setMusicButton.Text = "Set Music";
+            this.setMusicButton.UseVisualStyleBackColor = true;
             // 
             // MusicGameForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.setMusicButton);
+            this.Controls.Add(this.musicSelectionBox);
             this.Name = "MusicGameForm";
             this.Text = "Form1";
             this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.MusicGameForm_FormClosed);
@@ -43,6 +70,10 @@
         }
 
         #endregion
+
+        private System.Windows.Forms.Timer readMessageTimer;
+        private System.Windows.Forms.ComboBox musicSelectionBox;
+        private System.Windows.Forms.Button setMusicButton;
     }
 }
 
--- Visual Micro/.MusicGame.vsarduino.h
+++ Visual Micro/.MusicGame.vsarduino.h
@@ -44,7 +44,7 @@ extern "C" void __cxa_pure_virtual() {;}
 
 #include "C:\Program Files (x86)\Arduino\hardware\arduino\cores\arduino\arduino.h"
 #include "C:\Program Files (x86)\Arduino\hardware\arduino\variants\standard\pins_arduino.h" 
-#include "C:\Users\Thomas\Source\Repos\musicgame\MusicGame.ino"
-#include "C:\Users\Thomas\Source\Repos\musicgame\ButtonDebouncer.cpp"
-#include "C:\Users\Thomas\Source\Repos\musicgame\ButtonDebouncer.h"
+#include "D:\Dropbox\GitHub\musicgame\MusicGame.ino"
+#include "D:\Dropbox\GitHub\musicgame\ButtonDebouncer.cpp"
+#include "D:\Dropbox\GitHub\musicgame\ButtonDebouncer.h"
 #endif
