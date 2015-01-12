a9ad261cdc04f375ac69557c1d6d20357e9427ea
Thomas Schraven <t.schraven@student.fontys.nl>
2015-01-12 14:27:53 +0100

    added HasIntervalPassed() function, bugfixes

--- MusicGame/Utilities.h
+++ MusicGame/Utilities.h
@@ -1,7 +1,13 @@
+#ifndef Utilities_h
+#define Utilities_h
+
+#include <Arduino.h>
+
 int GetIndex(char *input, char character)
 {
 	const char *indexptr = strchr(input, character);
-	if (indexptr) {
+	if (indexptr)
+	{
 		return indexptr - input;
 	}
 	else
@@ -9,3 +15,16 @@ int GetIndex(char *input, char character)
 		return -1;
 	}
 }
+
+bool HasIntervalPassed(long &lasttime, int interval)
+{
+	if ((lasttime + interval) > millis())
+	{
+		return false;
+	}
+
+	lasttime = millis();
+	return true;
+}
+
+#endif
